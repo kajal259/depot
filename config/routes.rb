93e48a2ef461c4a9221e7723ba99db1e89528a83
kajal259 <kajal.logicalstreet@gmail.com>
2022-01-22 10:49:52 +0530

    I18n

--- config/routes.rb
+++ config/routes.rb
@@ -5,16 +5,23 @@ Rails.application.routes.draw do
     post 'login' => :create
     delete 'logout' => :destroy
   end
-  # get 'sessions/create'
-  # get 'sessions/destroy'
+  get 'sessions/create'
+  get 'sessions/destroy'
   resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-  root 'store#index', as: 'store_index'
-  resources :store
+  # resources :orders
+  # resources :line_items
+  # resources :carts
+  # root 'store#index', as: 'store_index'
+  # resources :store
   resources :products do
   get :who_bought, on: :member
+  end
+
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store_index', via: :all
   end
   # For details on the DSL available within this file, see
   # https://guides.rubyonrails.org/routing.html
